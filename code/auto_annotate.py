e47aa1b6e557e052cd546969f246c4898a45aff9
Shreyan Gupta <61325229+Xenon8778@users.noreply.github.com>
2024-04-08 17:02:21 -0500

    Add files via upload

--- code/auto_annotate.py
+++ code/auto_annotate.py
@@ -6,25 +6,59 @@ sc.settings.verbosity = 0
 import warnings
 warnings.filterwarnings('ignore')
 
-def auto_annot(adata, cluster, species = 'Mm', cutoff = 0.25):
+def auto_annot(adata, cluster, db = 'Panglao',
+               species = 'Mm', cutoff = 0.25):
     # Read anndata object
     data = adata.copy() 
 
-    # Load PanglaoDB Database
-    db = pd.read_table('https://raw.githubusercontent.com/Xenon8778/Auto_cell_annot/main/PanglaoDB_markers_27_Mar_2020.tsv', sep = '\t')
-    db_sub = db[['species','official gene symbol','cell type','ubiquitousness index']]
+    if db == 'Panglao':
+        # Load PanglaoDB Database
+        db = pd.read_table('https://raw.githubusercontent.com/Xenon8778/Auto_cell_annot/main/data/PanglaoDB_markers_27_Mar_2020.tsv', sep = '\t')
+        db_sub = db[['species','official gene symbol','cell type','ubiquitousness index']]
 
-    # Selecting species and subsetting database
-    db_sub = db_sub[(db_sub['species'] == species) | (db_sub['species'] == 'Mm Hs') | (db_sub['species'] == 'Hs Mm')]
-    db_sub = db_sub[db_sub['ubiquitousness index'] < 0.1]
+        # Selecting species and subsetting database
+        db_sub = db_sub[(db_sub['species'] == species) | (db_sub['species'] == 'Mm Hs') | (db_sub['species'] == 'Hs Mm')]
+        db_sub = db_sub[db_sub['ubiquitousness index'] < 0.1]
 
-    db_prep = []
-    for i in np.unique(db_sub['cell type']):
-        x1 = [x for x in db_sub[db_sub['cell type'] == i]['official gene symbol']]
-        if species == 'Mm':
-            x1 = [x.capitalize() for x in x1]
-        x2 = [i,x1]
-        db_prep.append(x2)
+        db_prep = []
+        for i in np.unique(db_sub['cell type']):
+            x1 = [x for x in db_sub[db_sub['cell type'] == i]['official gene symbol']]
+            if species == 'Mm':
+                x1 = [x.capitalize() for x in x1]
+            x2 = [i,x1]
+            db_prep.append(x2)
+
+    if db == 'CellMarker':
+
+        if (species == 'Mm'):
+            # Load CellMarker Database
+            db = pd.read_csv('https://github.com/Xenon8778/Auto_cell_annot/raw/main/data/Cell_marker_Human.csv')
+            db_sub = db[['speciesType','geneSymbol','cellName','tissueType']]
+
+            # Selecting species and subsetting database
+            db_sub = db_sub[(db_sub['species'] == species) | (db_sub['species'] == 'Mm Hs') | (db_sub['species'] == 'Hs Mm')]
+            db_sub = db_sub[db_sub['ubiquitousness index'] < 0.1]
+
+            db_prep = []
+            for i in np.unique(db_sub['cell_name']):
+                x1 = [x for x in db_sub[db_sub['cell_name'] == i]['Symbol']]
+                x2 = [i,x1]
+                db_prep.append(x2)
+            
+        else:
+            db = pd.read_csv('https://github.com/Xenon8778/Auto_cell_annot/raw/main/data/Cell_marker_Human.csv')
+
+            # Selecting species and subsetting database
+            db_sub = db_sub[(db_sub['species'] == species) | (db_sub['species'] == 'Mm Hs') | (db_sub['species'] == 'Hs Mm')]
+            db_sub = db_sub[db_sub['ubiquitousness index'] < 0.1]
+
+            db_prep = []
+            for i in np.unique(db_sub['cell_name']):
+                x1 = [x for x in db_sub[db_sub['cell_name'] == i]['Symbol']]
+                x2 = [i,x1]
+                db_prep.append(x2)
+
+    
 
     # Annotate each cluster 
     for i in tqdm(range(len(db_prep))):
